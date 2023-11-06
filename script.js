0fe3fbc467e85a999e664ae84a2c8e592301c4d2
bhoomika99h <147612670+bhoomika99h@users.noreply.github.com>
2023-11-06 23:30:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Example: Scroll to sections when clicking on the navigation links
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop - 50,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
